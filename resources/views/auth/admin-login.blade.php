7870e4352cafb813dbe0682f0112f5c019c75b30
Gerson Sousa <gersonsis2018@gmail.com>
2020-05-11 17:01:43 -0300

    6.1 - corrigido

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -205,27 +205,40 @@
                   
                     <div class="booking-form">
                         <h3>Bem Vindo</h3>
-                        <form method="POST" action="{{ route('admin.login.submit') }}">
+                        <form method="POST" action="{{ route('admin.login.submit') }}"> 
                             @csrf
                             <div class="check-date">
                                 <label for="email" class="label">Email:</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror                             
+                                @if($errors->has('email'))
+                                <div class="invalid-feedback">
+                            {{$errors->has('first')}}
+                                </div>
+                            @endif
+            
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror                            
                             </div>
+
                             <div class="check-date">
                                 <label for="Senha" class="label">Senha:</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                @if($errors->has('password'))
+                                <div class="invalid-feedback">
+                            {{$errors->has('first')}}
+                                </div>
+                            @endif
+            
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                                 
                             </div>
 
